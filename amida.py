33c6d34092d226a4e9e89619f6e36ae7f13f3793
hukurou-s <s18g455@stu.kagawa-u.ac.jp>
2018-05-24 01:06:53 +0900

    ランダムにあみだを生成する機能を実装

--- amida.py
+++ amida.py
@@ -1,3 +1,5 @@
+import random
+
 class Amida:
     people = 0
     times = 0
@@ -5,13 +7,26 @@ class Amida:
     def __init__(self, p, t):
         self.people = p
         self.times = t
-        self.lottery = [[False for i in range(p-1)] for j in range(t-1)]
+        self.lottery = [[False for i in range(p-1)] for j in range(t)]
+
 
     def draw_line(self, x, y):
         self.lottery[y][x] = True
 
-    def execute(self):
-        position = 0
+
+    def draw_random_line(self):
+        x_pos = random.choices(range(self.people-1), k=self.times)
+        for i in range(self.times):
+            self.draw_line(x_pos[i], i)
+
+
+    def show(self):
+        for i in self.lottery:
+            print(i)
+
+
+    def execute(self, pos):
+        position = pos
         self.show()
 
         for i in self.lottery:
@@ -32,19 +47,12 @@ class Amida:
 
         print(position)
 
-    def show(self):
-        for i in self.lottery:
-            print(i)
+
 
 amida = Amida(5, 5*2)
 
-amida.draw_line(0,0)
-amida.draw_line(1,1)
-amida.draw_line(2,2)
-amida.draw_line(3,3)
-amida.draw_line(3,4)
-amida.draw_line(2,5)
+amida.draw_random_line()
 
-amida.execute()
+amida.execute(0)
 
 
